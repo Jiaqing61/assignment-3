6f33e9e311a0fc3c5757c2cfe9abf82b1962856f
majalxs <162897811+majalxs@users.noreply.github.com>
2025-11-21 23:59:17 +0100

    update mirror mechanics

--- scripts/mirror.gd
+++ scripts/mirror.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 @export var rotation_degree: float = 15.0  
+@export var rotation_speed: float = 85.0  
 var player_touching: bool = false        
 
 
 func _process(delta):
 	if player_touching:
-		if Input.is_action_just_pressed("rotate_left"):
-			rotation_degrees -= rotation_degree
-		elif Input.is_action_just_pressed("rotate_right"):
-			rotation_degrees += rotation_degree
+		if Input.is_action_pressed("rotate_left"):
+			rotation_degrees -= rotation_speed * delta
+		elif Input.is_action_pressed("rotate_right"):
+			rotation_degrees += rotation_speed * delta
 
 
 func _on_Area2D_body_entered(body):
