c68786b3b1b165c09dcce51c665cd0e0b58915be
ttmmint <tanghaoran97@gmail.com>
2025-12-02 16:59:45 +0100

    fixed small bugs again

--- scripts/startmenu.gd
+++ scripts/startmenu.gd
@@ -1,9 +1,9 @@
 extends Control	
 
 func _on_start_pressed() -> void:
+	AudioManager.door_open_sfx.play()
 	get_tree().change_scene_to_file("res://Scenes/Levels/Level_1.tscn")
-	
-
 
 func _on_exit_pressed() -> void:
+	AudioManager.door_open_sfx.play()
 	get_tree().quit()
