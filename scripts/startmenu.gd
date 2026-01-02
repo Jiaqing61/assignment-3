ae01d697c85b5bae6d8a8da50099ed34890562e2
Yihui-Peng <pengyihui6633@163.com>
2026-01-02 16:57:42 +0100

    Change start scene to intro_scroll

--- scripts/startmenu.gd
+++ scripts/startmenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_start_pressed() -> void:
 	AudioManager.door_open_sfx.play()
-	get_tree().change_scene_to_file("res://scenes/Levels/Level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/intro_scroll.tscn")
 
 func _on_exit_pressed() -> void:
 	AudioManager.click_sfx.play()
